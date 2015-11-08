239961f77412d39ab006e67dd0c094c65351db4d
Aleix Pol <aleixpol@kde.org>
2015-11-08 22:55:44 +0100

    Fix Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-$EXTRACTRC `find muon/ -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> muonrc.cpp
-$XGETTEXT rc.cpp muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
+$EXTRACTRC `find src -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> muonrc.cpp
+$XGETTEXT rc.cpp muonrc.cpp `find src -name \*.cpp` -o $podir/muon.pot
 rm -f muonrc.cpp
