de92eb1d97cac3b94b7b247603d943189697176a
Pino Toscano <pino@kde.org>
2019-07-05 09:59:37 +0200

    i18n: use rc.cpp for extra sources
    Use rc.cpp as only dummy source for non-C strings, so repack-pot is
    happier.

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
 
-$EXTRACTRC `find src -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> muonrc.cpp
-$XGETTEXT rc.cpp muonrc.cpp `find src -name \*.cpp` -o $podir/muon.pot
-rm -f muonrc.cpp
+$EXTRACTRC `find src -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT rc.cpp `find src -name \*.cpp` -o $podir/muon.pot
